603f0470fd184f1f30d9c2af390991d1bb4cc042
유지우 <treeyu12345@naver.com>
2024-09-23 18:55:08 +0900

    FEAT :: [#5] Login Screen 추가

---
+++ app/src/main/java/com/teameeats/eeats/presentation/login/view/LoginScreen.kt
@@ -0,0 +1,18 @@
+package com.teameeats.eeats.presentation.login.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.teameeats.eeats.theme.EeatsColor
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize().background(EeatsColor.white),
+    ) {
+
+    }
+}
