1a6fa1a82b87939b7644e960093e66d7a138e00b
유지우 <treeyu12345@naver.com>
2024-09-23 18:54:44 +0900

    FEAT :: [#5] 네비게이션 기능 추가

---
+++ app/src/main/java/com/teameeats/eeats/utils/navigation/EeatsNavigation.kt
@@ -0,0 +1,25 @@
+package com.teameeats.eeats.utils.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.teameeats.eeats.presentation.login.view.LoginScreen
+import com.teameeats.eeats.presentation.splash.view.SplashScreen
+
+@Composable
+fun EeatsNavigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = EeatsRoutes.Splash.route,
+    ) {
+        composable(EeatsRoutes.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(EeatsRoutes.Login.route) {
+            LoginScreen(navController = navController)
+        }
+    }
+}
+
