c1087f239b93a7cd0e8d8162bb30b10ecc596dae
유지우 <treeyu12345@naver.com>
2024-09-23 19:18:47 +0900

    ADD :: ignore 처리

--- app/src/main/java/com/teameeats/eeats/presentation/login/view/LoginScreen.kt
+++ app/src/main/java/com/teameeats/eeats/presentation/login/view/LoginScreen.kt
@@ -2,7 +2,9 @@ package com.teameeats.eeats.presentation.login.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -10,9 +12,7 @@ import com.teameeats.eeats.theme.EeatsColor
 
 @Composable
 fun LoginScreen(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize().background(EeatsColor.white),
-    ) {
-
+    Column {
+        Box(modifier = Modifier.fillMaxSize().background(EeatsColor.white))
     }
 }
