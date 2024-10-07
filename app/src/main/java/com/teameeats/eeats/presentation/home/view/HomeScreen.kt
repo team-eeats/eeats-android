c676a2b75127b22a4e8dcd9e7c8ff92b09934d4f
유지우 <treeyu12345@naver.com>
2024-10-07 16:14:31 +0900

    FEAT :: [#7] Home Screen 추가

---
+++ app/src/main/java/com/teameeats/eeats/presentation/home/view/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.teameeats.eeats.presentation.home.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController) {
+
+}
